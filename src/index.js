cd6a419f8bd7039e060976f5ae79fe2fa4fc20df
HalynaHera <gerasymenko_ba12@nuwm.edu.ua>
2020-10-25 22:33:11 +0200

    feat: implemented reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0){
+        let arr = n.toString().split('').reverse();
+        delete(arr[arr.length -1]); 
+        return arr.join('');
+    }
+  return n.toString().split('').reverse().join('');
 }
